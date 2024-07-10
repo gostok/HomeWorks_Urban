1ed58516c54e272a4f99a5cf3ad0f03b79c8475f
Anton Vasilev <stiv4ik12012@gmail.com>
2024-07-10 09:45:31 +0300

    Update module_2_4.py

--- modul_2/module_2_4.py
+++ modul_2/module_2_4.py
@@ -1,34 +1,19 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
 primes = []
 not_primes = []
-a = 0 
 
-for i in range(len(numbers)):
-    is_prime = True 
-    a = numbers[i]
-    
-    # убираем число '1':
-    if a < 2:   
-        print(a) 
+for i in numbers:
+    if i == 1: # убираем единицу из перебора списка
         continue
-    
-    b = a ** (1 / 2)
-
-    for j in range(2, int(b + 1)):
-        if a % j == 0:
-            is_prime = False
+    is_primes = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_primes = False
             break
-        else:
-            is_prime = True
-            break
-
-    if not (is_prime):
-        not_primes.append(a)
+    if is_primes:
+        primes.append(i)
     else:
-        primes.append(a)
+        not_primes.append(i)
 
-
-
-print('Простые числа' , primes)
-print('Не простые числа' , not_primes)
+print(primes)
+print(not_primes)
