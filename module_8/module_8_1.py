0038d85522870305fe40327001a53ff8229354f2
Anton Vasilev <stiv4ik12012@gmail.com>
2024-07-28 10:12:22 +0300

    Add files via upload

---
+++ module_8/module_8_1.py
@@ -0,0 +1,15 @@
+
+
+def add_everything_up(a, b):
+    try:
+        result = a + b
+    except TypeError as te:
+        return str(a) + str(b)
+    else:
+        return result
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+
