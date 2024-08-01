f8ae1f3686267d3d7e2d41ae8476c636a952d71f
Anton Vasilev <stiv4ik12012@gmail.com>
2024-08-01 10:39:41 +0300

    Update module_9_7.py

--- module_9/module_9_7.py
+++ module_9/module_9_7.py
@@ -2,13 +2,16 @@
 def is_prime(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-
-        for i in range(2, int(result)):
-            if result % i == 0:
-                print('Составное')
+        if result <= 1:
+            print('Составное')
+        else:
+            for i in range(2, int(result)):
+                if result % i == 0:
+                    print('Составное')
+                    break
             else:
                 print('Простое')
-                break
+
         return result
 
     return wrapper
