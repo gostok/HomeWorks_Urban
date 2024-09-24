1b0d626dea54529d4b9ed78f9a2b6016ac5e16e0
Anton Vasilev <stiv4ik12012@gmail.com>
2024-09-24 11:05:45 +0300

    Update module_16_3.py

--- module_16/module_16_3/module_16_3.py
+++ module_16/module_16_3/module_16_3.py
@@ -12,14 +12,15 @@ async def get() -> dict:
     return users
 
 @app.post('/user/{username}/{age}')
-async def post(username: Annotated[str, Path(ge=4, le=15, description='Enter your username', example='Andrey')],
+async def post(username: Annotated[str, Path(min_length =4, max_length =15, description='Enter your username', example='Andrey')],
                age: int) -> str:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f"Имя: {username}, Возраст: {age}"
     return f'User {user_id} is registered'
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def put(username: str, age: Annotated[int, Path(ge=1, le=100, description='Enter your age', example='24')],
+async def put(username: Annotated[str, Path(min_length =4, max_length =15, description='Enter your username', example='Andrey')], 
+              age: Annotated[int, Path(ge=1, le=100, description='Enter your age', example='24')],
               user_id: int=Path(ge=1, le=20, description='Enter user_id', example='2')) -> str:
     users[user_id] = f"Имя: {username}, Возраст: {age}"
     return f"The user {user_id} is updated"
